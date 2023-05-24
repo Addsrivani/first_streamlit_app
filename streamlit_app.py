1953b92855e27e62a3876e1ba5e7575cafc16af8
Addsrivani <134353544+Addsrivani@users.noreply.github.com>
2023-05-23 23:58:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy meal')
 
 import requests
-fruityvice_response=request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
