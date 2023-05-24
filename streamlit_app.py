f27da5f319fd2a97394a08242ef4eb4cee67d7a1
Addsrivani <134353544+Addsrivani@users.noreply.github.com>
2023-05-24 00:00:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy meal')
-
+import panda
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
